ec992ccc40fb358064024f34f162ce03929861a1
Oleh Sharko <124092267+Wassermin21@users.noreply.github.com>
2026-05-14 20:32:43 +0300

    upload files

---
+++ script.js
@@ -0,0 +1,15 @@
+const animatedSections = document.querySelectorAll("[data-animate]");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+animatedSections.forEach((section) => observer.observe(section));
